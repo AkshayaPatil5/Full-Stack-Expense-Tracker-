7c97fcf806b7d42d0084bb287033c32ce29546bd
AkshayaPatil5 <akshayapatil1987@gmail.com>
2024-01-24 14:19:15 +0530

    gitfile

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//imports
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -7,14 +6,12 @@ const cors = require('cors');
 const path = require('path');
 
 
-//models
 const expense = require('./model/expensemodel')
 const users = require('./model/userdetails')
 const order = require('./model/order')
 const Forgotpassword = require('./model/forgotpassword');
 
 
-//routes
 const mainpageroute = require('./routes/mainpageroute')
 const user = require('./routes/user')
 const expenseroute = require('./routes/expense')
@@ -22,16 +19,13 @@ const purchase = require('./routes/purchase')
 const resetpassword = require('./routes/resetpassword')
 
 
-//middlewares
 app.use(cors())
 app.use(express.json())
 
 
-//static serving 
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//redirection
 app.use(mainpageroute)
 app.use('/user', user)
 app.use('/expense', expenseroute)
@@ -39,7 +33,6 @@ app.use('/purchase', purchase)
 app.use('/password', resetpassword)
 
 
-// Define associations
 users.hasMany(expense);
 expense.belongsTo(users);
 
@@ -50,13 +43,10 @@ users.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(users);
 
 
-
-
-//this is to intialise database tables and then start the servers
 sequelize.sync({})
     .then((result) => {
 
-        app.listen(process.env.PORT);
+        app.listen(process.env.DB_PORT);
     })
     .catch((err) => {
         console.log(err);
